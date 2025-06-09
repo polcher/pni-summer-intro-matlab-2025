0cb352d51e5c75b2a23f8f1bf98aa901bc76193c
polcher <158093184+polcher@users.noreply.github.com>
2025-06-09 17:29:05 -0400

    Add files via upload

---
+++ week2/week_2_tues.m
@@ -0,0 +1,41 @@
+%% Arrays 
+% write down an matrix of numbers (using variable naming guidelines)
+
+% write down a string of characters
+
+% write down a cell array consisting of your matrix and your string
+
+%% Matrices
+% Use the following matrix:
+my_matrix = [2 5 4; 1 7 8; 10 11 12];
+
+% Select the value 7 from this matrix using row and column indexing
+
+
+% Select [4,8,12] from this matrix using row and column indexing
+
+
+% Select the value 7 from this matrix using one index
+
+
+%% 3 Dimensional matrices 
+% Use the following matrix:
+my_3D_matrix(:, :, 1) = [1 2; 3 4; 5 6];
+my_3D_matrix(:, :, 2) = [7 8; 9 10; 11 12];
+my_3D_matrix(:, :, 3) = [13 14; 15 16; 17 18];
+
+% Select the value 3 from this matrix
+
+% Select [13 15 17] from this matrix
+
+% Select [3,6] from this matrix
+
+%% Define two matrices, concatenate and multiply
+
+% Create a 3X4 nans matrix and randi matrix and concatenate them in the row dimension. 
+% Create a 3X4 ones matrix and randi matrix and concatenate them in the column dimension.
+% Take the transpose of the randi matrix and multiply the two matrices (non-element-wise)
+% Reshape the randi matrix into a 6x2 matrix
+
+
+
