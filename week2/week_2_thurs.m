26c435f8f7a44a44d736f4eb9236aa41c5d98b8f
polcher <158093184+polcher@users.noreply.github.com>
2025-06-12 11:45:27 -0400

    Add files via upload

---
+++ week2/week_2_thurs.m
@@ -0,0 +1,25 @@
+%%% In-class exercises
+
+%% Exercise 1
+% Make a function that takes in the radius of a circle and returns its circumference
+% For reference, circumfrence = 2 * pi * radius
+
+
+%% Exercise 2
+% Make a function that takes in a vector of elements and returns the sum of its unique elements
+
+
+%% Exercise 3
+% Make a function that takes in a vector of elements and returns its mean, standard deviation, 
+% min, max, and unique elements, all as separate variables.
+
+
+%% Exercise 4
+% Write a function that takes in a temperature in Fahrenheit and returns two outputs: 
+% the converted temperature in Celsius and the converted temperature in Kelvin
+% For reference, C = 5/9 * (F − 32) and K = C + 273.15
+
+
+%% Exercise 5
+% Write a function that takes in an array of temperatures in Fahrenheit and returns 
+% both the average and the range of these temperatures in Celsius.
