ba9874c5179e4b6bb9de49fe15c30fa2f8047ab8
polcher <158093184+polcher@users.noreply.github.com>
2025-06-16 14:53:39 -0400

    Add files via upload

---
+++ week3/week_3_tues.m
@@ -0,0 +1,46 @@
+%% Exercise 1
+% 1. Create an array of integers going from 20 to 1 with a step of 2. 
+
+
+% 2. Find what integer is at index 4. 
+
+
+% 3. Find what index the number 6 is in this array. 
+
+
+%% Exercise 2
+% 1. create a 3X4 matrix using the rand() function.
+
+
+% 2. find indices of the elements greater or equal to 0.5
+
+
+% 3. replace these elements with nan.
+
+
+% Challenge: replace all the numbers in your matrix which are smaller than 0.3 with
+% 100. Do not use find(). 
+
+
+%% Exercise 3
+% create a matrix of 8x8 random integers up to value 10.
+
+
+% find indices of elements bigger than 3 and smaller than 7. 
+
+
+% find indices of elements either smaller than 2 or bigger than 9. 
+
+
+% make these into a function that receives a matrix as an input and outputs two arrays
+% of indices. 
+
+
+%% Exercise 4
+% 1. How can you test if at least any of the elements in a matrix are not 0?
+
+%2. How can you test if all the elements in a matrix are not 0? 
+
+
+
+
